f2c2f2b218e265d94ca45f3f233e39d751654d4a
Cem KOÇ <koc.huseyincem@gmail.com>
2022-05-23 21:23:37 +0300

    create TCP Server class

---
+++ server/TCPServer.h
@@ -0,0 +1,18 @@
+#ifndef TCPSERVER_H
+#define TCPSERVER_H
+
+#include <cstdint>
+#include <string>
+
+class TCPServer {
+public:
+    TCPServer(uint16_t port, std::string (*read_callback)(std::string income_data));
+    uint16_t get_port();
+    ~TCPServer();
+
+private:
+    uint16_t port;
+    std::string (*read_callback)(std::string);
+};
+
+#endif
